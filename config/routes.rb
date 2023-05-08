712e9dd56d71b2480353d9f8fa03d7c569ab166e
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-08 00:22:01 +0000

    [Finish] 5-10

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   
-  resources :post_iamges, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :index, :show, :create]
   get '/about' => 'homes#about', as:'about'
   
   
